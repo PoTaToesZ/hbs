4463afcb089608f9d05ca22cf6410c4513d4a222
potato <leducanh27773@gmail.com>
2022-08-03 11:01:56 +0700

    update

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require('express')
 const Handlebars = require('handlebars')
 var app = express()
 var hbs = require('hbs')
+var fs = require('fs')
 
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
@@ -14,6 +15,16 @@ hbs.registerHelper("mbold", function(options) {
 });
 
 app.get('/', (req, res) => {
+    let content = fs.readFileSync('product.txt', 'utf8').trim().split('\n')
+    let ls = []
+    content.forEach(element => {
+        let p = {
+            name: element.split(';')[0],
+            color: element.split(';')[1],
+            price: element.split(';')[2]
+        }
+        ls.push(p)
+    });
     var name = "Potato"
     var products = ['Vovol','Honda','Toyota']
     var products2 = [
@@ -29,7 +40,7 @@ app.get('/', (req, res) => {
         }
     ]
     //truyen bien name vao trang home
-    res.render('home',{'name': name,'products': products,'products2': products2})
+    res.render('home',{'name': name,'products': products,'products2': products2,'content': ls})
 })
 
 const PORT = process.env.PORT || 5000
